7fb1801401a76a484796d0bed5ef32b82b7d31d5
David <david@callbook.ai>
2025-07-16 09:43:30 -0500

    handle core error on disconnecting websocket

--- src/pipecat/transports/network/fastapi_websocket.py
+++ src/pipecat/transports/network/fastapi_websocket.py
@@ -62,6 +62,7 @@ class FastAPIWebsocketClient:
         self._is_binary = is_binary
         self._callbacks = callbacks
         self._disconnect_lock = asyncio.Lock()
+        self._is_disconnecting = False
 
     def receive(self) -> typing.AsyncIterator[bytes | str]:
         return self._websocket.iter_bytes() if self._is_binary else self._websocket.iter_text()
@@ -74,11 +75,13 @@ class FastAPIWebsocketClient:
                 await self._websocket.send_text(data)
 
     async def disconnect(self):
-        async with self._disconnect_lock:
-            if self.is_connected and not self.is_closing:
-                self._closing = True
-                await self._websocket.close()
-                await self.trigger_client_disconnected()
+        if self._is_disconnecting: 
+            return
+
+        self._is_disconnecting = True 
+        if self._websocket.client_state == WebSocketState.CONNECTED:
+            logger.debug("Closing client websocket")
+            await self._websocket.close()
 
     async def trigger_client_disconnected(self):
         await self._callbacks.on_client_disconnected(self._websocket)
